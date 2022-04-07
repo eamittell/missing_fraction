de292d6dc3699c2b10bad78b5771e98f93400c30
eamittell <61084822+eamittell@users.noreply.github.com>
2022-04-07 13:49:17 +0100

    Create data_simulation_half_sibs.R

---
+++ data_simulation_half_sibs.R
@@ -0,0 +1,86 @@
+# Libraries required
+library(mvtnorm)
+
+## SIMULATION SETUP HALF SIBS ##
+# Residual covariance -> phenotypic covariance trait 2 0.4(G) + 0.4(R) = 0.8(P)
+cvr <- 0.4
+betas <- c(-0.9,-0.8,-0.7,-0.6,-0.5,-0.4,-0.3,-0.2,-0.05,0,0.05,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9)
+n.sires <- 500 
+n.offspring <- 5
+
+# Generate phenotypes
+d <- expand.grid(sire=1:n.sires,o=1:n.offspring)
+d <- d[order(d$sire),]
+d$sire <- paste("S",d$sire,sep="")
+d$dam <- paste("D",1:(n.sires*n.offspring),sep="") # Half sibs
+d$id <- paste("O",1:(n.sires*n.offspring),sep="")
+
+# Half sibs 
+n.dams <- n.sires*n.offspring
+
+# Organise the data into a pedigree, add dam and sire records before offspring
+pedigree <- d[,c("id","sire","dam")]
+dam.p <- data.frame(id=unique(d$dam),sire=NA,dam=NA)
+pedigree <- rbind(dam.p,pedigree)
+sire.p <- data.frame(id=unique(d$sire),sire=NA,dam=NA)
+pedigree <- rbind(sire.p,pedigree)
+
+# Genetic covariance 0.4
+G <- matrix(c(0.5,0.4,0.4,0.5),2,2)
+
+# Residual variance-covariance matrix
+R <- matrix(c(0.5,cvr,cvr,0.5),2,2)
+
+# Phenotypic var-covar matrix
+P <- G + R
+
+# Breeding values for each trait
+a <- rbv(pedigree, G)
+a1 <- a[3001:5500,] # Half sibs
+d$a1 <- a1[,1]
+d$a2 <- a1[,2]
+
+# Check 1
+a[3001:3010,] # Half sibs
+d[1:10,]
+
+# Check 2: offspring on single parent regression of a should be 0.5 (so roughly 1 if we multiple by 2)
+d$father_a1 <- a[match(d$sire,dimnames(a)[[1]]),1]
+d$mother_a1 <- a[match(d$dam,dimnames(a)[[1]]),1]
+summary(lm(a1~father_a1,data=d))$coefficients[2,]
+coef(lm(a1~father_a1,data=d))[2]*2
+summary(lm(a1~mother_a1,data=d))$coefficients[2,]
+coef(lm(a1~mother_a1,data=d))[2]*2
+
+# Residual effects
+r <- rmvnorm(n.sires*n.offspring,c(0,0),R)
+rownames(r) <- d$id # Add id
+d$r1 <- r[,1]
+d$r2 <- r[,2]
+
+# Compose phenotypes
+d$z1 <- d$a1 + d$r1
+d$z2 <- d$a2 + d$r2
+
+# Survival
+inv.logit <- function(x){exp(x)/(1+exp(x))}
+surv_func <- function(x,a=0,b=beta){inv.logit(a+b*x)}
+d$W1 <- rbinom(dim(d)[1],1,surv_func(d$z1)) # Fitness at viability stage
+
+# Later-life trait phenotypes
+d$z2_obs <- d$z2
+d$z2_obs[which(d$W1==0)] <- NA # Don't observe later-life trait for those individuals that do not survive viability stage
+
+# Later-life trait breeding values
+d$a2_obs <- d$a2
+d$a2_obs[which(d$W1==0)] <- NA
+
+# Consequence for trait 2
+d$W2 <- rpois(length(d$z1),exp(0+0.25*d$z2))
+# Total lifetime fitness
+d$W <- d$W2*d$W1
+# Fitness observed for trait 2
+d$W_obs <- d$W
+
+str(d)
+summary(d)
