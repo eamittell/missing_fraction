b643ee5a72d852093d1998a9dd6a51b689129c27
eamittell <61084822+eamittell@users.noreply.github.com>
2023-11-19 13:44:45 +0000

    Update episodes_of_selection_analysis.R

--- episodes_of_selection_analysis.R
+++ episodes_of_selection_analysis.R
@@ -1,3 +1,6 @@
+# Covariance-variance matrix z1, all z2 and values observed for z2
+covar <- cov(d[,c("z1","z2","z2_obs")],use="pairwise.complete.obs")
+
 # Composite function to calculate the prior viability selection differential from the 
 # logistic intercept and slope (a and b), trait mean and sd (m and s)
 S_logistic <- function(a,b,m,s){
@@ -13,22 +16,15 @@ S_logistic <- function(a,b,m,s){
 	return((integrate(f=ffun,lower=low,upper=up,a=a,b=b,m=m,s=s)$value-m*muW)/muW)
 }
 
-# Infinite population
-# Viability selection on trait 1
+# Phenotypic viability selection on trait 1
+# In an infinite population across known parameters
 S1a_inf <- S_logistic(0,beta,0,sqrt(P[1,1]))
-# calculation of what prior viability selection on trait 2 should be
-S2a_inf <- S1a_inf/(P[1,1])*P[1,2]
+# Calculation of what prior viability selection on trait 2 should be
+S2a_inf <- S1a_inf/(P[2,2])*P[1,2]
 
-# Simulated population
-# Phenotypic S on trait 1
+# Phenotypic S on trait 1 in simulated data
+# Known if measured early-life traits and viability selection in a focal population
 S1a <- weighted.mean(d$z1,d$W1)-mean(d$z1)
-# Phenotypic prior sel diff trait 2
-S2a <- weighted.mean(d$z2,d$W1)-mean(d$z2)
-# total selection of trait 2, but with missing fraction problem -- i.e. fecundity selection on z2
-d6 <- subset(d,d$W1==1)
-S2b <- weighted.mean(d6$z2,d6$W)-mean(d6$z2)
-# total selection of trait 2, if we had perfect knowledge
-S2t <- weighted.mean(d$z2,d$W)-mean(d$z2)
 
 library(norm2)
 # The variances and covariances estimated using expectation maximum likelihood
@@ -37,7 +33,16 @@ sigma2 <- summary(ML_P)$param$sigma
 # Covariance-variance matrix z1 and values observed for z2 
 sigma2
 
-# True
-covar <- cov(d[,c("z1","z2","z2_obs")],use="pairwise.complete.obs")
+# Estimated phenotypic prior selection differential trait 2
+S2a_est <- (S1a/sigma2[2,2])*sigma2[1,2]
 
-### Same analyses and results for half-sibs ###
+# Variance of survivors from first to second episode
+V2b <- P[2,2] - S2a_logˆ2
+
+# Fecundity selection differential on later-life trait of survivors
+# Remembering that selection differential = selection gradient (beta)*variance
+S2b <- beta2*V2b
+# Total selection of trait 2
+S2t <- S2a_log + S2b
+# Total estimated selection of trait 2
+S2t_est <- S2a_est + S2b
